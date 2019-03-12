970e0fca0654e3f2c6db492ff60e26b632780572
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-12 16:59:26 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,11 +4,11 @@ def hello(array)
   i = 0
   collection = []
   while i < array.length
-    binding.pry
-    collection << yield(array[i])
+    colleciton << yield(array[i])
     i += 1
   end
   collection
 end
+
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
